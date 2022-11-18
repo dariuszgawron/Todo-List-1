a210060a9f2b9ee778c8084c6e96e89f507f9b16
Dariusz Gawron <screbsy+g1@gmail.com>
2022-11-18 22:26:18 +0100

    Preparation of the structure for the forms

--- src/components/AddCategoryForm/AddCategoryForm.js
+++ src/components/AddCategoryForm/AddCategoryForm.js
@@ -2,11 +2,14 @@ import React from "react";
 
 import './AddCategoryForm.scss';
 
-const AddCategoryForm = () => {
+const AddCategoryForm = (props) => {
     return (
-        <div>
-
-        </div>
+        <form className="category-form" id="new-category-form">
+            <div className="category-form__submit">
+                <i className="category-form__submit-icon fa-solid fa-plus"></i>
+            </div>
+            <input className="category-form__input" type="text" placeholder="Add new list" id="new-category-value" required/>
+        </form>
     )
 };
 
